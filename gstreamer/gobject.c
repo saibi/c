c3d60e867a55c2f3d7a0224ed0377f150f4dbdb9
Youngmin Kim <ymkim@huvitz.com>
2019-05-31 18:08:01 +0900

    gstreamer elements

---
+++ gstreamer/gobject.c
@@ -0,0 +1,23 @@
+#include <gst/gst.h>
+
+int main(int argc, char *argv[])
+{
+	GstElement *element;
+
+	gchar *name;
+
+
+	gst_init(&argc, &argv);
+
+	element = gst_element_factory_make("fakesrc", "source");
+
+	/* get name */
+
+	g_object_get(G_OBJECT(element), "name", &name, NULL);
+	g_print("The name of the element is '%s'.\n", name);
+	g_free(name);
+
+	gst_object_unref(GST_OBJECT(element));
+
+	return 0;
+}
