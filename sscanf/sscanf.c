59b5874cf67b395d9c3adf729de0e4d5e73c7fa1
Youngmin Kim <kimyoungmin@gmail.com>
2020-10-08 15:16:57 +0900

    sscanf unsigned long

--- sscanf/sscanf.c
+++ sscanf/sscanf.c
@@ -15,11 +15,16 @@ int main(void)
 {
 	char ch = 0;
 	int val = -1;
+	unsigned long data_size = 0;
 
 	//sscanf("#B T I$", "#B T %c=%d$", &ch, &val);
 	sscanf("#B T L=0$", "#B T %c=%d$", &ch, &val);
 
 	printf("ch = %c, val = %d\n", ch, val);
+
+	sscanf("upgrade 123456789$", "upgrade %lu$", &data_size); 
+
+	printf("data_size = %lu\n", data_size);
 	return 0;
 }
 /********** end of file **********/
