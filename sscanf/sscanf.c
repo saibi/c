a3afdbff4ab98fa51f3808be743e19b7a6d1d754
new5244 <new5244@gmail.com>
2020-10-14 13:47:56 +0900

    sscanf %ld

--- sscanf/sscanf.c
+++ sscanf/sscanf.c
@@ -16,6 +16,7 @@ int main(void)
 	char ch = 0;
 	int val = -1;
 	unsigned long data_size = 0;
+	long long_val = 0;
 
 	//sscanf("#B T I$", "#B T %c=%d$", &ch, &val);
 	sscanf("#B T L=0$", "#B T %c=%d$", &ch, &val);
@@ -25,6 +26,10 @@ int main(void)
 	sscanf("upgrade 123456789$", "upgrade %lu$", &data_size); 
 
 	printf("data_size = %lu\n", data_size);
+
+	sscanf("18", "%ld", &long_val);
+	printf("long_val = %ld\n", long_val);
+
 	return 0;
 }
 /********** end of file **********/
