ea3e7ff5ce163842a4874d5db36d958cc7f9b0c2
Youngmin Kim <kimyoungmin@gmail.com>
2022-05-16 16:48:06 +0900

    strncpy

--- strncpy/strncpy.c
+++ strncpy/strncpy.c
@@ -4,11 +4,18 @@
 int main(void)
 {
 	char buf1[4];
-
+	char src[8] = {0, };
+	char dest[9] = {0, };
 
 	strncpy(buf1, "abcd", 4);
 
 	printf("strncpy test, [3] = %d\n", buf1[3]);
+
+	strncpy(src, "hello123", 8);
+	printf("src [%s]\n", src);
+
+	strncpy(src, "world", 5);
+	printf("src [%s]\n", src);
 
 	return 0;
 }
