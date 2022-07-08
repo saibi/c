75c5c0b5261d0c13d3c2b35a6430962b91fbc045
Youngmin Kim <kimyoungmin@gmail.com>
2022-07-08 11:02:59 +0900

    snprintf

--- snprintf/snprintf.c
+++ snprintf/snprintf.c
@@ -2,14 +2,19 @@
 
 int main(void)
 {
-	int ret ;
 	char buf[4];
+	int ret ;
+	char buf_clean[4] = { 0, };
 
 	ret = snprintf(buf, sizeof(buf), "hello %.2f world !!!", 18.18);
+	printf("buf ret = %d\n", ret);
 
-	printf("ret = %d\n", ret);
+	ret = snprintf(buf_clean, sizeof(buf_clean), "hello %.2f world !!!", 18.18);
+	printf("buf_clean ret = %d\n", ret);
+
 	printf("[1234567890]\n");
-	printf("[%s]\n", buf);
+	printf("[%s] buf\n", buf);
+	printf("[%s] buf_clean\n", buf_clean);
 
 	return 0;
 }
